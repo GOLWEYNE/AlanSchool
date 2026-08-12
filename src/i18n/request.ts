7e895ad55828c7dd0f962a1fc1408f9768d65b20
GOLWEYNE <32921055+GOLWEYNE@users.noreply.github.com>
2026-08-13 03:50:17 +0700

    fix: remove hasLocale import not available in installed next-intl version

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,13 +1,16 @@
-import { hasLocale } from "next-intl";
 import { getRequestConfig } from "next-intl/server";
 import { cookies } from "next/headers";
 import { routing } from "./routing";
 
+type AppLocale = (typeof routing.locales)[number];
+
+function isAppLocale(value: string | undefined): value is AppLocale {
+  return !!value && (routing.locales as readonly string[]).includes(value);
+}
+
 export default getRequestConfig(async () => {
   const cookieLocale = (await cookies()).get("NEXT_LOCALE")?.value;
-  const locale = hasLocale(routing.locales, cookieLocale)
-    ? cookieLocale
-    : routing.defaultLocale;
+  const locale = isAppLocale(cookieLocale) ? cookieLocale : routing.defaultLocale;
 
   return {
     locale,
