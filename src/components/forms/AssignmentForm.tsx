7f9267aea90628215ce1ea49b83a17fc4acd037c
GOLWEYNE <32921055+GOLWEYNE@users.noreply.github.com>
2026-08-24 12:01:17 +0500

    dark: fix contrast for AssignmentForm

--- src/components/forms/AssignmentForm.tsx
+++ src/components/forms/AssignmentForm.tsx
@@ -59,7 +59,7 @@ const AssignmentForm = ({
 
   return (
     <form className="flex flex-col gap-8" onSubmit={onSubmit}>
-      <h1 className="text-xl font-semibold">
+      <h1 className="text-xl font-semibold dark:text-blue-100">
         {type === "create" ? t("assignment.createTitle") : t("assignment.updateTitle")}
       </h1>
 
@@ -106,9 +106,9 @@ const AssignmentForm = ({
           />
         )}
         <div className="flex flex-col gap-2 w-full md:w-1/4">
-          <label className="text-xs text-gray-500">{t("assignment.lesson")}</label>
+          <label className="text-xs text-gray-500 dark:text-slate-400">{t("assignment.lesson")}</label>
           <select
-            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            className="ring-[1.5px] ring-gray-300 dark:ring-slate-700 dark:bg-slate-800 dark:text-slate-100 p-2 rounded-md text-sm w-full"
             {...register("lessonId")}
             defaultValue={data?.lessonId}
           >
