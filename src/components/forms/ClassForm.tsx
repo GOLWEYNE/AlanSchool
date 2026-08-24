b88d14d9096063b6a612b9386fe43f0a018424ed
GOLWEYNE <32921055+GOLWEYNE@users.noreply.github.com>
2026-08-24 12:02:26 +0500

    dark: fix contrast for ClassForm

--- src/components/forms/ClassForm.tsx
+++ src/components/forms/ClassForm.tsx
@@ -70,7 +70,7 @@ const ClassForm = ({
 
   return (
     <form className="flex flex-col gap-8" onSubmit={onSubmit}>
-      <h1 className="text-xl font-semibold">
+      <h1 className="text-xl font-semibold dark:text-blue-100">
         {type === "create" ? t("class.createTitle") : t("class.updateTitle")}
       </h1>
 
@@ -100,9 +100,9 @@ const ClassForm = ({
           />
         )}
         <div className="flex flex-col gap-2 w-full md:w-1/4">
-          <label className="text-xs text-gray-500">{t("class.supervisor")}</label>
+          <label className="text-xs text-gray-500 dark:text-slate-400">{t("class.supervisor")}</label>
           <select
-            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            className="ring-[1.5px] ring-gray-300 dark:ring-slate-700 dark:bg-slate-800 dark:text-slate-100 p-2 rounded-md text-sm w-full"
             {...register("supervisorId")}
             defaultValue={data?.teachers}
           >
@@ -125,9 +125,9 @@ const ClassForm = ({
           )}
         </div>
         <div className="flex flex-col gap-2 w-full md:w-1/4">
-          <label className="text-xs text-gray-500">{t("common.grade")}</label>
+          <label className="text-xs text-gray-500 dark:text-slate-400">{t("common.grade")}</label>
           <select
-            className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full"
+            className="ring-[1.5px] ring-gray-300 dark:ring-slate-700 dark:bg-slate-800 dark:text-slate-100 p-2 rounded-md text-sm w-full"
             {...register("gradeId")}
           >
             {grades.map((grade: { id: number; level: number }) => (
